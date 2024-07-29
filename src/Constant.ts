f1a51d6ecd0a5539c72c878292ccc1ceb0d8dd50
topaznoam <noamtopaz@gmail.com>
2024-07-29 20:34:37 +0300

    refactor-constants

---
+++ src/Constant.ts
@@ -0,0 +1,5 @@
+export const DATABASE_HOST = 'localhost';
+export const DATABASE_PORT = 5432;
+export const DATABASE_USERNAME = 'postgres';
+export const DATABASE_PASSWORD = 'admin';
+export const DATABASE_NAME = 'chat_database';

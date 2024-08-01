6f6701b61cc5bd1671b9b370b2dfe3b4beb56e8d
topaznoam <noamtopaz@gmail.com>
2024-08-01 13:35:37 +0300

    feat messages basic logic and sockets

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Param, Body, Delete } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { Messages } from './messages';
+import { CreateMessageDto } from './create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -17,8 +18,8 @@ export class MessagesController {
   }
 
   @Post()
-  create(@Body() message: Messages): Promise<Messages> {
-    return this.messagesService.create(message);
+  create(@Body() createMessageDto: CreateMessageDto): Promise<Messages> {
+    return this.messagesService.create(createMessageDto);
   }
 
   @Delete(':id')
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,13 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Messages } from './messages';
+import { User } from '../users/users';
+import { Group } from '../group/groups';
+import { CreateMessageDto } from './create-message.dto';
 
 @Injectable()
 export class MessagesService {
   constructor(
     @InjectRepository(Messages)
     private readonly messagesRepository: Repository<Messages>,
+
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
+    @InjectRepository(Group)
+    private readonly groupRepository: Repository<Group>,
   ) {}
 
   async findAll(): Promise<Messages[]> {
@@ -18,7 +27,25 @@ export class MessagesService {
     return this.messagesRepository.findOne({ where: { id } });
   }
 
-  async create(message: Messages): Promise<Messages> {
+  async create(createMessageDto: CreateMessageDto): Promise<Messages> {
+    const user = await this.userRepository.findOne({
+      where: { id: createMessageDto.user },
+    });
+    const group = await this.groupRepository.findOne({
+      where: { id: createMessageDto.group },
+    });
+
+    if (!user || !group) {
+      throw new NotFoundException('User or Group not found');
+    }
+
+    const message = new Messages();
+    message.data = createMessageDto.data;
+    message.time = new Date().toLocaleTimeString();
+    message.date = new Date();
+    message.user = user;
+    message.group = group;
+
     return this.messagesRepository.save(message);
   }
 
